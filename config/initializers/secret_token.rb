bea5bba61e8cf6c8f04b49350422478ce8b64ee4
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2014-04-03 16:50:22 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JeopardyPrototype::Application.config.secret_key_base = 'd0cd59ff8779bb9bf97192ab99e9a0010612860a258ae8b3d1fdb5d3bc9ee27a417088a5f45efe193b9391c657bccf36e85b7bdf0b61a786e5e057ef109db0cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JeopardyPrototype::Application.config.session_store :cookie_store, key: '_jeopardy-prototype_session'
